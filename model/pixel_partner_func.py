10f1818ba6235ced215f5fbf1036b866d1027c9f
iwu_78 <keledang777@gmail.com>
2023-10-23 15:21:51 -0700

    fixed vertical merge support for combine function

--- model/pixel_partner_func.py
+++ model/pixel_partner_func.py
@@ -28,25 +28,38 @@ def pixelate(image, size):
 def combine(b641, b642, direction, resample=Image.BICUBIC, resize_big_image=True):
     im1 = base64toImage(b641)
     im2 = base64toImage(b642)
-    if im1.height == im2.height:
-        _im1 = im1
-        _im2 = im2
-    elif (((im1.height > im2.height) and resize_big_image) or
-          ((im1.height < im2.height) and not resize_big_image)):
-        _im1 = im1.resize((int(im1.width * im2.height / im1.height), im2.height), resample=resample)
-        _im2 = im2
-    else:
-        _im1 = im1
-        _im2 = im2.resize((int(im2.width * im1.height / im2.height), im1.height), resample=resample)
-    dst = Image.new('RGB', (_im1.width + _im2.width, _im1.height))
-    dst.paste(_im1, (0, 0))
-    if direction == "horizontal":
+    if direction == "Horizontal":
+        if im1.height == im2.height:
+            _im1 = im1
+            _im2 = im2
+        elif (((im1.height > im2.height) and resize_big_image) or
+            ((im1.height < im2.height) and not resize_big_image)):
+            _im1 = im1.resize((int(im1.width * im2.height / im1.height), im2.height), resample=resample)
+            _im2 = im2
+        else:
+            _im1 = im1
+            _im2 = im2.resize((int(im2.width * im1.height / im2.height), im1.height), resample=resample)
+        dst = Image.new('RGB', (_im1.width + _im2.width, _im1.height))
+        dst.paste(_im1, (0, 0))
         dst.paste(_im2, (_im1.width, 0))
     else:
+        if im1.width == im2.width:
+            _im1 = im1
+            _im2 = im2
+        elif (((im1.width > im2.width) and resize_big_image) or
+            ((im1.width < im2.width) and not resize_big_image)):
+            _im1 = im1.resize((im2.width, int(im1.height * im2.width / im1.width)), resample=resample)
+            _im2 = im2
+        else:
+            _im1 = im1
+            _im2 = im2.resize((im1.width, int(im2.height * im1.width / im2.width)), resample=resample)
+        dst = Image.new('RGB', (_im1.width, _im1.height + _im2.height))
+        dst.paste(_im1, (0, 0))
+        dst.paste(_im2, (0, _im1.height))
         dst.paste(_im2, (0, _im1.height))
     return imageToBase64(dst)
 
 
-# Test Pixel Partner Model
+# Test Pixel Model
 if __name__ == "__main__": 
     print("~~~Debug~~~")
